985d4434ddcf5c0fd5f302c905c260317740b09a
devinferdynand <jobsdevin2@gmail.com>
2026-08-19 21:07:01 +0700

    FIX-BUG-19-agustus-2026

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,6 @@ Route::middleware('auth')->group(function () {
     // Proses Logout
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::prefix('master/supplier')->name('supplier.')->group(function () {
-    Route::get('/', [SupplierController::class, 'index'])->name('index');
-    Route::get('/data', [SupplierController::class, 'data'])->name('data');
-    Route::post('/', [SupplierController::class, 'store'])->name('store');
-    Route::put('/{spid}', [SupplierController::class, 'update'])->name('update');
-    Route::patch('/{spid}/toggle-aktif', [SupplierController::class, 'toggleAktif'])->name('toggle-aktif');
-});
     // Halaman Dashboard Utama
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
@@ -60,36 +53,36 @@ Route::prefix('master/supplier')->name('supplier.')->group(function () {
             Route::patch('/{stid}/toggle-aktif', [SatuanController::class, 'toggleAktif'])->name('toggle-aktif');
         });
     });
-});
 
-Route::prefix('transaksi/permintaan-barang')->name('permintaan-barang.')->group(function () {
-    Route::get('/', [PermintaanBarangController::class, 'index'])->name('index');
-    Route::get('/data', [PermintaanBarangController::class, 'data'])->name('data');
-    Route::get('/form-options', [PermintaanBarangController::class, 'formOptions'])->name('form-options');
-    Route::get('/create', [PermintaanBarangController::class, 'create'])->name('create');
-    Route::get('/{pbid}/edit', [PermintaanBarangController::class, 'edit'])->name('edit');
-    Route::get('/{pbid}', [PermintaanBarangController::class, 'show'])->name('show');
-    Route::post('/', [PermintaanBarangController::class, 'store'])->name('store');
-    Route::put('/{pbid}', [PermintaanBarangController::class, 'update'])->name('update');
-    Route::patch('/{pbid}/status', [PermintaanBarangController::class, 'updateStatus'])->name('status');
-});
+    Route::prefix('transaksi/permintaan-barang')->name('permintaan-barang.')->group(function () {
+        Route::get('/', [PermintaanBarangController::class, 'index'])->name('index');
+        Route::get('/data', [PermintaanBarangController::class, 'data'])->name('data');
+        Route::get('/form-options', [PermintaanBarangController::class, 'formOptions'])->name('form-options');
+        Route::get('/create', [PermintaanBarangController::class, 'create'])->name('create');
+        Route::get('/{pbid}/edit', [PermintaanBarangController::class, 'edit'])->name('edit');
+        Route::get('/{pbid}', [PermintaanBarangController::class, 'show'])->name('show');
+        Route::post('/', [PermintaanBarangController::class, 'store'])->name('store');
+        Route::put('/{pbid}', [PermintaanBarangController::class, 'update'])->name('update');
+        Route::patch('/{pbid}/status', [PermintaanBarangController::class, 'updateStatus'])->name('status');
+    });
 
-Route::prefix('transaksi/pesanan-pembelian')->name('pesanan-pembelian.')->group(function () {
-    Route::get('/', [PesananPembelianController::class, 'index'])->name('index');
-    Route::get('/data', [PesananPembelianController::class, 'data'])->name('data');
-    Route::get('/form-options', [PesananPembelianController::class, 'formOptions'])->name('form-options');
-    Route::get('/create', [PesananPembelianController::class, 'create'])->name('create');
-    Route::get('/dari-permintaan/{pbid}', [PesananPembelianController::class, 'dariPermintaan'])->name('dari-permintaan');
-    Route::get('/{ppid}', [PesananPembelianController::class, 'show'])->name('show');
-    Route::post('/', [PesananPembelianController::class, 'store'])->name('store');
-    Route::patch('/{ppid}/status', [PesananPembelianController::class, 'updateStatus'])->name('status');
-});
+    Route::prefix('transaksi/pesanan-pembelian')->name('pesanan-pembelian.')->group(function () {
+        Route::get('/', [PesananPembelianController::class, 'index'])->name('index');
+        Route::get('/data', [PesananPembelianController::class, 'data'])->name('data');
+        Route::get('/form-options', [PesananPembelianController::class, 'formOptions'])->name('form-options');
+        Route::get('/create', [PesananPembelianController::class, 'create'])->name('create');
+        Route::get('/dari-permintaan/{pbid}', [PesananPembelianController::class, 'dariPermintaan'])->name('dari-permintaan');
+        Route::get('/{ppid}', [PesananPembelianController::class, 'show'])->name('show');
+        Route::post('/', [PesananPembelianController::class, 'store'])->name('store');
+        Route::patch('/{ppid}/status', [PesananPembelianController::class, 'updateStatus'])->name('status');
+    });
 
-Route::prefix('transaksi/penerimaan-barang')->name('penerimaan-barang.')->group(function () {
-    Route::get('/', [PenerimaanBarangController::class, 'index'])->name('index');
-    Route::get('/data', [PenerimaanBarangController::class, 'data'])->name('data');
-    Route::get('/form-options', [PenerimaanBarangController::class, 'formOptions'])->name('form-options');
-    Route::get('/create', [PenerimaanBarangController::class, 'create'])->name('create');
-    Route::get('/{pnid}', [PenerimaanBarangController::class, 'show'])->name('show');
-    Route::post('/', [PenerimaanBarangController::class, 'store'])->name('store');
+    Route::prefix('transaksi/penerimaan-barang')->name('penerimaan-barang.')->group(function () {
+        Route::get('/', [PenerimaanBarangController::class, 'index'])->name('index');
+        Route::get('/data', [PenerimaanBarangController::class, 'data'])->name('data');
+        Route::get('/form-options', [PenerimaanBarangController::class, 'formOptions'])->name('form-options');
+        Route::get('/create', [PenerimaanBarangController::class, 'create'])->name('create');
+        Route::get('/{pnid}', [PenerimaanBarangController::class, 'show'])->name('show');
+        Route::post('/', [PenerimaanBarangController::class, 'store'])->name('store');
+    });
 });
